a6ee365af2a6ce1547743a3733339c1adbf4d5f1
MohamedMorabet <medmrbt5@gmail.com>
2023-06-19 15:58:50 -0700

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - program that prints all possible different combinations of 2 digits
- * Return: 0
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
+	char letter;
 
-	while (c <= 99)
-	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-		if (f_d < l_d)
-		{
-			putchar(f_d);
-			putchar(l_d);
+	putchar('\n');
 
-			if (c != 89)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-		c++;
-	}
-putchar('\n');
-return (0);
+	return (0);
 }
