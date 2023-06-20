fcc28469ba3a0d9fac0b2a4ad41b2805ca51315f
MohamedMorabet <medmrbt5@gmail.com>
2023-06-20 12:39:32 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,11 +4,8 @@
  * main - Entry point
  *Return: Always 0
  */
-
 int main(void)
-        
-{       
-        
+{               
         _putchar('_');
         _putchar('p');
         _putchar('u');
@@ -19,5 +16,4 @@ int main(void)
         _putchar('r');
         _putchar('\n');
         return (0);
-
 }
